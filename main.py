9491c478bcb2c7a14377d5c03067c9f2b7d6a133
Katie Ford <katie@katieford.io>
2023-01-22 14:59:44 -0600

    add light validation for inputs and formatting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
 
 class BucketRunner(object):
     """
@@ -10,7 +12,7 @@ class BucketRunner(object):
     """
     iterations = 0
 
-    def __init__(self, bucket_a_size, bucket_b_size, goal):
+    def __init__(self, bucket_a_size: int, bucket_b_size: int, goal: int):
         self.big_bucket = Bucket(max(bucket_a_size, bucket_b_size))
         self.small_bucket = Bucket(min(bucket_a_size, bucket_b_size))
         self.goal = goal
@@ -24,28 +26,34 @@ class BucketRunner(object):
         keep transferring big to small until you solve the problem.V
         """
 
-        print("Buckets are sized {} and {}. Goal is {}".format(self.big_bucket.max_volume, self.small_bucket.max_volume, self.goal))
+        print("Buckets are sized {} and {}. Goal is {}".format(
+            self.big_bucket.max_volume, self.small_bucket.max_volume,
+            self.goal))
 
-        while self.big_bucket.current_volume != self.goal and self.small_bucket.current_volume != self.goal:
+        while self.big_bucket.current_volume != self.goal and \
+                self.small_bucket.current_volume != self.goal:
             if self.iterations > 10000:
                 raise Exception("yikes! I think something went wrong")
             if self.big_bucket.is_empty():
-                print("\tBig bucket is empty. Filling to {} units".format(self.big_bucket.max_volume))
+                print("\tBig bucket is empty. Filling to {} units".format(
+                    self.big_bucket.max_volume))
                 self.big_bucket.fill_from_lake()
             if self.small_bucket.is_full():
                 print("\tSmall bucket is full. Dumping")
                 self.small_bucket.dump()
             self.small_bucket.fill_from_bucket(self.big_bucket)
             self.iterations += 1
-            print("Big bucket: {}, small bucket: {}".format(self.big_bucket.current_volume, self.small_bucket.current_volume))
+            print("Big bucket: {}, small bucket: {}".format(
+                self.big_bucket.current_volume,
+                self.small_bucket.current_volume))
+        print("Goal volume acquired in {} steps".format(self.iterations))
 
 
 class Bucket(object):
     max_valume = 0
     current_volume = 0
 
-
-    def __init__(self, size):
+    def __init__(self, size: int):
         """
         A bucket has two kinds of attributes: static attributes that are
         inherent to the object (such as the size of the bucket) and dynamic
@@ -53,6 +61,8 @@ class Bucket(object):
         the former when initializing the object
 
         """
+        if size > 99:
+            raise ValueError("maximum bucket size is 99")
         self.max_volume = size
 
     def fill_from_lake(self):
@@ -86,6 +96,7 @@ class Bucket(object):
 
     def is_empty(self):
         return self.current_volume == 0
+
 
 if __name__ == '__main__':
     BucketRunner(3, 5, 4).execute_optimal()
