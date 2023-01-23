d1c45adc6534787abee98f1f405488f024dc8db5
Katie Ford <katie@katieford.io>
2023-01-22 20:05:29 -0600

    add gitignore and readme

--- src/api.py
+++ src/api.py
@@ -1,12 +1,18 @@
+""" Water Buckets API"""
 import json
 
 import hug
 
 from bucket import BucketRunner
 
+@hug.cli()
 @hug.get(examples='bucket_a_size=3&bucket_b_size=5&goal=4')
 @hug.local()
 def get_bucket_transfer_steps(bucket_a_size, bucket_b_size, goal):
+    """
+    Gets the actions necessary to use the available buckets to get to the goal
+    volume
+    """
     runner = None
     try:
         runner = BucketRunner(int(bucket_a_size), int(bucket_b_size), int(goal))
@@ -17,3 +23,6 @@ def get_bucket_transfer_steps(bucket_a_size, bucket_b_size, goal):
             }
     except Exception as err:
         return {'error': err}
+
+if __name__ == '__main__':
+    get_bucket_transfer_steps.interface.cli()
